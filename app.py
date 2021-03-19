29aa2f84e02f849d5050cffd89e1e632ed0c6fb9
sidkulk <kulksud@gmail.com>
2021-03-19 14:33:20 +0530

    registration and DB created

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from flask import Flask, render_template, request
-from flask_table import Table, Col
 from flask_sqlalchemy import SQLAlchemy
+# from wtforms import StringField, PasswordField
+# from wtforms.fields.core import BooleanField
+# from wtforms.validators import InputRequired, Email, Length
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
-
+app.config['SECRET_KEY'] = 'thisissupposedtobesecret'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 app.config['SQLALCHEMY_TRACK_NOTIFICATIONS'] = False
+Bootstrap(app)
 db = SQLAlchemy(app)
 
 class Register(db.Model):
@@ -20,7 +24,7 @@ class Register(db.Model):
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    return render_template('register.html')
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -28,15 +32,19 @@ def register():
         username = request.form['username']
         email = request.form['email']
         password = request.form['password']
-        user = Register(username=username, email=email, password=password)
-        db.session.add(user)
-        db.session.commit()
-        return 'Successfully registered!'
+        if username=="" or email =="" or password == "":
+            return render_template('error.html')
+        else:
+            user = Register(username=username, email=email, password=password)
+            db.session.add(user)
+            db.session.commit()
+            return 'Successfully registered!'
 
 @app.route('/users')
 def users():
     users = Register.query.all()
     return render_template('users.html', users=users)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
