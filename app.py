f057a94e98decca6320ee6b5a689cad8f469b9c3
sidkulk <kulksud@gmail.com>
2021-03-21 18:13:31 +0530

    project reset with added login page

--- app.py
+++ app.py
@@ -1,50 +1,65 @@
-from flask import Flask, render_template, request
-from flask_sqlalchemy import SQLAlchemy
-# from wtforms import StringField, PasswordField
-# from wtforms.fields.core import BooleanField
-# from wtforms.validators import InputRequired, Email, Length
-from flask_bootstrap import Bootstrap
+from flask import Flask, render_template, request, session, logging, url_for, redirect, flash
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
 
+from passlib.hash import sha256_crypt
+
+engine = create_engine("sqlite:///test.db")
+db = scoped_session(sessionmaker(bind=engine))
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'thisissupposedtobesecret'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-app.config['SQLALCHEMY_TRACK_NOTIFICATIONS'] = False
-Bootstrap(app)
-db = SQLAlchemy(app)
 
-class Register(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20))
-    email = db.Column(db.String(30))
-    password = db.Column(db.String(20))
+@app.route("/")
+def home():
+    return render_template('home.html')
 
-    def __repr__(self) -> str:
-        return f"Register('{self.username}', '{self.email}')"
-
-
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    return render_template('register.html')
-
-@app.route('/register', methods=['GET', 'POST'])
+#register
+@app.route('/register', methods=["GET", "POST"])
 def register():
     if request.method == 'POST':
-        username = request.form['username']
-        email = request.form['email']
-        password = request.form['password']
-        if username=="" or email =="" or password == "":
-            return render_template('error.html')
+        name = request.form.get("username")
+        email = request.form.get("email")
+        password = request.form.get("password")
+        cpassword = request.form.get("cpassword")
+        securepass = sha256_crypt.encrypt(str(password))
+
+        if password == cpassword:
+            db.execute("INSERT into REGISTER(username, email, password) VALUES(:name, :email, :password)", {"name":name, "email":email, "password":securepass})
+            db.commit()
+            flash("Account created!", "success")
+            return redirect(url_for('login'))
         else:
-            user = Register(username=username, email=email, password=password)
-            db.session.add(user)
-            db.session.commit()
-            return 'Successfully registered!'
+            flash("password doesn't match", "danger")
+            return render_template("register.html")
+    return render_template('register.html')
 
-@app.route('/users')
-def users():
-    users = Register.query.all()
-    return render_template('users.html', users=users)
+#login
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    if request.method == 'POST':
+        username = request.form.get("username")
+        password = request.form.get("password")
 
+        usernamedata = db.execute("SELECT username FROM REGISTER WHERE username=:username", {"username":username}).fetchone()
+        passwordata = db.execute("SELECT password FROM REGISTER WHERE username=:username", {"username":username}).fetchone()
+
+        if usernamedata is None:
+            flash("No user found!", "danger")
+            return render_template("login.html")
+        else:
+            for pass_data in passwordata:
+                if sha256_crypt.verify(password, pass_data):
+                    flash("Login successful!", "success")
+                    return redirect(url_for('dash'))
+                else:
+                    flash("Incorrect Password", "danger")
+                    return render_template("login.html")
+
+    return render_template('login.html')
+
+@app.route('/dash')
+def dash():
+    return render_template("dash.html")
 
 if __name__ == '__main__':
+    app.secret_key="kulksidtestkey"
     app.run(debug=True)
